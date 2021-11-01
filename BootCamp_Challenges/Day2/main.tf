c945f1ea563fdf1c7bd6d26f3bc0982040a74361
Omkar Birader <omkar.birader@microsoft.com>
2021-11-01 21:10:47 +0530

    Added two vms

--- BootCamp_Challenges/Day2/main.tf
+++ BootCamp_Challenges/Day2/main.tf
@@ -49,9 +49,10 @@ resource "oci_core_route_table" "test_route_table" {
 }
 
 resource "oci_core_instance" "compute_instance" {
+    count = 2
     availability_domain = var.instance_availability_domain
     compartment_id = var.ComputeCompartment
-    display_name = "Webserver1"
+    display_name = "Webserver${count.index}"
     shape = "VM.Standard.E2.1.Micro"
     create_vnic_details {
         subnet_id = oci_core_subnet.contoso_subnet.id        
@@ -63,6 +64,6 @@ resource "oci_core_instance" "compute_instance" {
       source_id = var.compute_image_id
     }
     metadata = {
-        ssh_authorized_keys = "<Publickey>"
+        ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwZXW0mTCvNGVN8UZS8vRcyPBajDt9AB10ZHn7Dsooz4jXbmCx4OIyavzmqlfBbMuYXVYGTCbHk28f5Rpb+yrRQJct9ckQaB2HTcE8vPZCGEYz4xFbhI2Ls67VfrtJSk6kxAAHW8rKz/aZzSp2G0GSsooAJDrSg9Y2RPqnHCd9XRcbAFe3vFdJ9Wp1gTP5Y/V3KagLY1C3Y6/xJWXCpalboEZxb0OTvhxJ6gHgw2SG7cgs1LJZyUbjlBdc6C8FJ3p3n8LDfzu3GF0K+aofVaqNKAJdgfibGWdIfKXuOntGdJias6D4CWsJFUHr38sVdaqNbkp1PDqDf/pkl4SDzWRRsFZJJe0RVIJ06O7ZoJaDRJ9uWsIfEH/NF9yBmb2Z2NZFkSUz+7HStYRFO3Ic9TkKa9Fy+1RPJBZC1oiYO0Gmp2vLNbIyVk5WnoqjGDnc8ih2uzUd4sHytwkX1DHFd7LPMEWGRL71L404iHjqGlF0zt4233Lx76wgW0V3IxxRdss= fareast\\ombirade@MININT-7V3ASPE"
     }
 }
